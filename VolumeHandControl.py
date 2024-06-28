e3e9a8a93f09c5438f5ada74699cce62a7e0abe7
Nithilan-r <95030971+Nithilan-r@users.noreply.github.com>
2024-06-28 23:42:21 +0530

    Add files via upload

---
+++ VolumeHandControl.py
@@ -0,0 +1,70 @@
+import cv2
+import time
+import numpy as np
+import HandTrackingModule as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+wCam,hCam=640,480
+
+
+cap=cv2.VideoCapture(0)
+cap.set(3,wCam)
+cap.set(4,hCam)
+pTime=0
+
+detector=htm.handDetector(detectionCon=0.7)
+
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = interface.QueryInterface(IAudioEndpointVolume)
+#volume.GetMute()
+#volume.GetMasterVolumeLevel()
+volRange=volume.GetVolumeRange()
+
+minVol=volRange[0]
+maxVol=volRange[1]
+vol=0
+
+
+while True:
+    success,img=cap.read()
+    img=detector.findHands(img)
+    lmlist=detector.findPosition(img,draw=False)
+    if(lmlist!=0):
+        print(lmlist[4],lmlist[8])
+
+        x1,y1=lmlist[4][1],lmlist[4][2]
+        x2,y2=lmlist[8][1],lmlist[8][2]
+        cx,cy=(x1+x2)//2,(y1+y2)//2
+        cv2.circle(img,(x1,y1),15,(255,0,255),cv2.FILLED)
+        cv2.circle(img,(x2,y2),15,(255,0,255),cv2.FILLED)
+        cv2.line(img,(x1,y1),(x2,y2),(255,0,255),3)
+        cv2.circle(img,(cx,cy),15,(255,0,255),cv2.FILLED)
+
+        length=math.hypot(x2-x1,y2-y1)
+        #print(length)
+
+        #hand range 50-300
+        #vol range -65-0
+        #used for range conversion
+        vol=np.interp(length,[50,300],[minVol,maxVol])
+        print(vol)
+        volume.SetMasterVolumeLevel(vol, None)
+
+        if length<50:
+            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+
+    cv2.rectangle(img,(50,150),(85,400),(0,255,0),3)
+    #cv2.rectangle(int,(50,int(vol)),(85,400),(0,255,0),cv2.FILLED)
+
+    cTime=time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
+    cv2.putText(img,str(int(fps)),(40,50),cv2.FONT_HERSHEY_COMPLEX,3,(255,0,255),3)
+    cv2.imshow("Image",img)
+    cv2.waitkey(1)
